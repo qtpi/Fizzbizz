6ceb63138560472e2f56190c9f348840c4c95a71
brandito <branditoway@gmail.com>
2013-02-12 15:51:11 -0800

    Collapsed code to one line, still not complicated enough.

--- fizzbizz.rb
+++ fizzbizz.rb
@@ -1,13 +1,5 @@
 #!/usr/bin/env ruby -w
 
 (1..100).each do |n|
-  STDOUT.puts (
-    (n % 15) == 0 ?
-      "Fizzbuzz" :
-    (n % 3) == 0 ?
-      "Fizz" :
-    (n % 5) == 0 ?
-      "Buzz" :
-    n
-  )
+  STDOUT.puts(( n % 15 ) == 0 ? "Fizzbuzz" : ( n % 5 ) == 0 ? "Buzz" : ( n % 3 ) == 0 ? "Fizz" : (n.to_s))
 end
