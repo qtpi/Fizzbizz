b4b37ce3fb2bff964c08d978176f6beb317a0f0c
brandito <branditoway@gmail.com>
2013-02-12 16:44:45 -0800

    Fairly complicated, achieved a more workable point.

--- fizzbizz.rb
+++ fizzbizz.rb
@@ -1,13 +1,14 @@
-def fizzy(iterations)
-  f = ->(n) { (n % 3) == 0 ? "fizz" : nil }
-  b = ->(n) { (n % 5) == 0 ? "buzz" : nil }
-  x = ->(n) { (f[n] and b[n]) ? f[n] + b[n] : (f[n] or b[n]) or n }
+f = ->(n) { (n % 3) == 0 ? [102, 105, 122, 122] : nil }
+b = ->(n) { (n % 5) == 0 ? [98, 117, 122, 122] : nil }
+x = ->(n) { (f[n] and b[n]) ? f[n] + b[n] : (f[n] or b[n]) or n }
+
+(1..20).each do |n|
+	p x[n]
 end
 
-fizzy(15)
-fizzy(10)
-fizzy(9)
-fizzy(1)
+#x[15].each do |c|
+#  puts [c].pack('c')
+#end
 
 #(1..100).each do |n|
 #  puts(( n % 15 ) == 0 ? "Fizzbuzz" : ( n % 5 ) == 0 ? "Buzz" : ( n % 3 ) == 0 ? "Fizz" : (n.to_s))
