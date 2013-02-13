80fbd10e0bd9d3acd7fd19a760da85f682fc72f7
brandito <branditoway@gmail.com>
2013-02-12 17:10:34 -0800

    This is one gnarly piece piece of code.

--- fizzbizz.rb
+++ fizzbizz.rb
@@ -1,9 +1,15 @@
-f = ->(n) { (n % 3) == 0 ? [102, 105, 122, 122] : nil }
-b = ->(n) { (n % 5) == 0 ? [98, 117, 122, 122] : nil }
+# to_str
 s = ->(c) { c.kind_of?(Array) ? c.map(&:chr).join.to_s : c.to_s }
+# fizz
+f = ->(n) { (n % 3) == 0 ? [102, 105, 122, 122] : nil }
+# buzz
+b = ->(n) { (n % 5) == 0 ? [98, 117, 122, 122] : nil }
+# fizzbizz
 x = ->(n) { (f[n] and b[n]) ? f[n] + b[n] : (f[n] or b[n]) or n }
+# main iteration
+m = ->(m) {
+  (iter = ->(max, n=1, list=[]) { n <= max ? iter[max, n + 1, list << s[x[n]]] : list })[m]
+}
 
-puts s[x[15]]
-puts s[x[9]]
-puts s[x[7]]
-puts s[x[5]]
+# call it
+puts m[100]
