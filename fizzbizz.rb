ff0ef86d920358e2f78bdcf6c19e2e8ba115a1c9
brandito <branditoway@gmail.com>
2013-02-12 16:21:54 -0800

    Slightly more complicated

--- fizzbizz.rb
+++ fizzbizz.rb
@@ -1,5 +1,14 @@
-#!/usr/bin/env ruby -w
-
-(1..100).each do |n|
-  STDOUT.puts(( n % 15 ) == 0 ? "Fizzbuzz" : ( n % 5 ) == 0 ? "Buzz" : ( n % 3 ) == 0 ? "Fizz" : (n.to_s))
+def fizzy(iterations)
+  f = ->(n) { (n % 3) == 0 ? "fizz" : nil }
+  b = ->(n) { (n % 5) == 0 ? "buzz" : nil }
+  x = ->(n) { (f[n] and b[n]) ? f[n] + b[n] : (f[n] or b[n]) or n }
 end
+
+fizzy(15)
+fizzy(10)
+fizzy(9)
+fizzy(1)
+
+#(1..100).each do |n|
+#  puts(( n % 15 ) == 0 ? "Fizzbuzz" : ( n % 5 ) == 0 ? "Buzz" : ( n % 3 ) == 0 ? "Fizz" : (n.to_s))
+#end
