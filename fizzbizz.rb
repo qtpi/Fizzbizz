b78165251464b540b629eb21aa24778f012f33df
brandito <branditoway@gmail.com>
2013-02-12 16:53:34 -0800

    Nearly everything completed, all's left is to create a function or loop

--- fizzbizz.rb
+++ fizzbizz.rb
@@ -1,15 +1,9 @@
 f = ->(n) { (n % 3) == 0 ? [102, 105, 122, 122] : nil }
 b = ->(n) { (n % 5) == 0 ? [98, 117, 122, 122] : nil }
+s = ->(c) { c.kind_of?(Array) ? c.map(&:chr).join.to_s : c.to_s }
 x = ->(n) { (f[n] and b[n]) ? f[n] + b[n] : (f[n] or b[n]) or n }
 
-(1..20).each do |n|
-	p x[n]
-end
-
-#x[15].each do |c|
-#  puts [c].pack('c')
-#end
-
-#(1..100).each do |n|
-#  puts(( n % 15 ) == 0 ? "Fizzbuzz" : ( n % 5 ) == 0 ? "Buzz" : ( n % 3 ) == 0 ? "Fizz" : (n.to_s))
-#end
+puts s[x[15]]
+puts s[x[9]]
+puts s[x[7]]
+puts s[x[5]]
